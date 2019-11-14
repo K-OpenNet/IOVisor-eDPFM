ce304e564ca6e7c96d97653715535694ba9dd7da
gist-banana <yechoe@smartx.kr>
2019-11-14 03:04:21 +0000

    uploading

---
+++ kafka_consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+import sys
+
+bootstrap_servers = ['localhost:9091','localhost:9092','localhost:9090']
+topicName = 'xdp_kafka_topic'
+
+consumer = KafkaConsumer (topicName, group_id = 'group1', bootstrap_servers = bootstrap_servers, auto_offset_reset = 'earliest')
+
+try:
+    for message in consumer:
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+
+except KeyboardInterrupt:
+    sys.exit()
