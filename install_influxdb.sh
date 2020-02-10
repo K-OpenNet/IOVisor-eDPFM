d6dbfc494c7847e639adf75193c9b102998cdac4
gist-banana <gist.banana@gist.ac.kr>
2020-02-10 09:35:31 +0000

    install_influxdb.sh added

---
+++ install_influxdb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo Installing InfluxDB!
+
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+
+source /etc/lsb-release
+
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt update
+sudo apt install influxdb -y
+sudo systemctl start influxdb
+sudo systemctl enable influxdb
+
+echo "execute [$netstat -plntu] and check influxdb running"
+
+
--- test/ip_decimal_to_human.py
+++ test/ip_decimal_to_human.py
@@ -1,9 +1,10 @@
 from ast import literal_eval
 
-INPUT_VALUE = 134744072 # This value should be 8.8.8.8 after conversion
-INPUT_VALUE2 = 3232235777
+INPUT_VALUE = str(134744072) # This value should be 8.8.8.8 after conversion
+INPUT_VALUE2 = str(3232235777)
 
 def decimal_to_human(input_value):
+    input_value = int(input_value)
     hex_value = hex(input_value)[2:]
     print(str(hex_value) + '\n')
     pt3 = literal_eval((str('0x'+str(hex_value[-2:]))))
@@ -11,10 +12,12 @@ def decimal_to_human(input_value):
     pt1 = literal_eval((str('0x'+str(hex_value[-6:-4]))))
     pt0 = literal_eval((str('0x'+str(hex_value[-8:-6]))))
     print(str('pt0 :')  + '\n' + str(pt0) + '\n' + str('pt1 :') + str(pt1) + '\n' + str('pt2 :') + str(pt2) + '\n' + str('pt3: ') + str(pt3))
+    result = str(pt0)+'.'+str(pt1)+'.'+str(pt2)+'.'+str(pt3)
+    return result
 
 print('\n\n\n')
 print('first try : ')
-decimal_to_human(INPUT_VALUE2)
+print(decimal_to_human(INPUT_VALUE2))
 print('\n')
 print('second try : ')
-decimal_to_human(INPUT_VALUE)
+print(decimal_to_human(INPUT_VALUE))
