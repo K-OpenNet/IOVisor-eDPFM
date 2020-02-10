7fd16aa4effc79055df5164583b636379b9e6b31
gist-banana <gist.banana@gist.ac.kr>
2020-02-10 08:20:49 +0000

    now need to attach database to kafka_packet_monitor_rx.py

--- eBPF/kafka_packet_monitor_rx.py
+++ eBPF/kafka_packet_monitor_rx.py
@@ -14,10 +14,27 @@ def kafka_consumer():
     try :
         for message in consumer:
             value = message.value
+            '''
             if (value[0] != '0'):
                 if (value != '9999999'):
                     value = str(value)
                     print(decimal_to_human(value))
+            '''
+            # value being saved appropriately has been confirmed
+            # data is either parsed in 3 columns or 6 columns -> len : 30 && len : 60
+            if (len(value) == 30):
+#                print(value)
+                src_ip_0 = decimal_to_human(str(value[9:19]))
+                dst_ip_0 = decimal_to_human(str(value[20:30]))
+                print(' src1 : ' + str(src_ip_0) + 'dst1 : ' + str(dst_ip_0))
+                
+            elif (len(value) == 60):
+#                print(value)
+                src_ip_0 = decimal_to_human(str(value[9:19]))
+                dst_ip_0 = decimal_to_human(str(value[20:30]))
+                src_ip_1 = decimal_to_human(str(value[39:49]))
+                dst_ip_1 = decimal_to_human(str(value[50:60]))
+                print(' src1 : ' + str(src_ip_0) + ' dst1 : ' + str(dst_ip_0) + ' src2 : '+ str(src_ip_1) + ' dst2 : ' + str(dst_ip_1))
     except KeyboardInterrupt:
         sys.exit()
 
