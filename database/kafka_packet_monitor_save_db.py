b4f7b9f2b395c9a52d14e8ff01c3f6e89ad10773
gist-banana <gist.banana@gist.ac.kr>
2020-02-11 07:12:40 +0000

    kafka_packet_monitor_Save_db works

--- database/kafka_packet_monitor_save_db.py
+++ database/kafka_packet_monitor_save_db.py
@@ -1,6 +1,7 @@
 from kafka import KafkaConsumer
 import sys
 from ast import literal_eval
+from pymongo import MongoClient
 
 #define information about kafka
 
@@ -9,32 +10,41 @@ topicName = 'packetmonitor'
 
 consumer = KafkaConsumer(topicName, bootstrap_servers = bootstrap_servers)
 
+# database test - BEGIN
+
+def insert_into_db(src_ip, dst_ip, time):
+    client = MongoClient()
+    db = client.test    # database name
+    collection = db.test    # document name
+    collection.insert_one({'src_ip':src_ip,'dst_ip':dst_ip,'time':time})
+
+# database test - END
+
 def kafka_consumer():
     print("kafka consumer test...")
     try :
         for message in consumer:
             value = message.value
-            '''
-            if (value[0] != '0'):
-                if (value != '9999999'):
-                    value = str(value)
-                    print(decimal_to_human(value))
-            '''
             # value being saved appropriately has been confirmed
             # data is either parsed in 3 columns or 6 columns -> len : 30 && len : 60
             if (len(value) == 30):
 #                print(value)
-                src_ip_0 = decimal_to_human(str(value[9:19]))
-                dst_ip_0 = decimal_to_human(str(value[20:30]))
+                src_ip_0 = str(decimal_to_human(str(value[9:19])))
+                dst_ip_0 = str(decimal_to_human(str(value[20:30])))
                 print(' src1 : ' + str(src_ip_0) + 'dst1 : ' + str(dst_ip_0))
+                time_temp = '111'
+                insert_into_db(src_ip_0,dst_ip_0,time_temp)
                 
             elif (len(value) == 60):
 #                print(value)
-                src_ip_0 = decimal_to_human(str(value[9:19]))
-                dst_ip_0 = decimal_to_human(str(value[20:30]))
-                src_ip_1 = decimal_to_human(str(value[39:49]))
-                dst_ip_1 = decimal_to_human(str(value[50:60]))
+                time_temp = '111'
+                src_ip_0 = str(decimal_to_human(str(value[9:19])))
+                dst_ip_0 = str(decimal_to_human(str(value[20:30])))
+                src_ip_1 = str(decimal_to_human(str(value[39:49])))
+                dst_ip_1 = str(decimal_to_human(str(value[50:60])))
                 print(' src1 : ' + str(src_ip_0) + ' dst1 : ' + str(dst_ip_0) + ' src2 : '+ str(src_ip_1) + ' dst2 : ' + str(dst_ip_1))
+                insert_into_db(src_ip_0,dst_ip_0,time_temp)
+                insert_into_db(src_ip_1,dst_ip_1,time_temp)
     except KeyboardInterrupt:
         sys.exit()
 
@@ -48,4 +58,6 @@ def decimal_to_human(input_value):
     result = str(pt0)+'.'+str(pt1)+'.'+str(pt2)+'.'+str(pt3)
     return result
 
+
 kafka_consumer()
+
