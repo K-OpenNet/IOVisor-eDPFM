b2f6bdf176cdb28976e236d4e5a35b1cf0247d23
root <root@gist-cye-post>
2020-03-10 10:33:11 +0000

    backup organized

--- database/stat_n_packet_monitor/kafka_packet_monitor_save_db.py
+++ database/stat_n_packet_monitor/kafka_packet_monitor_save_db.py
@@ -5,10 +5,10 @@ from pymongo import MongoClient
 import pytz
 #define information about kafka
 
-bootstrap_servers = ['210.125.84.133:9092']
-topicName = 'packetmonitor'
+bootstrap_servers_bpf2 = ['210.125.84.133:9092']
+topicName_bpf2 = 'packetmonitor'
 
-consumer = KafkaConsumer(topicName, bootstrap_servers = bootstrap_servers)
+consumer_bpf2 = KafkaConsumer(topicName, bootstrap_servers = bootstrap_servers_bpf2)
 
 # database test - BEGIN
 
@@ -23,17 +23,9 @@ def insert_into_db(src_ip, pkt_num, time):
 def kafka_consumer():
     print("kafka consumer test...")
     try :
-        for message in consumer:
+        for message in consumer_bpf2:
             value = message.value
-            # value being saved appropriately has been confirmed
-            # data is either parsed in 3 columns or 6 columns -> len : 30 && len : 60
-#            print(value)
-#            print('\n\n len : ')
-#            print(value)
             length = len(value)
-#                src_ip = decimal_to_human(str(value[i*22:(i+1)*22-11]))   # first ten digits parsed
-#                dst_ip = decimal_to_human(str(value[(i+1)*22-11:(i+1)*22]))  # second ten digits parsed
-#                insert_into_db(src_ip, dst_ip, '111')
             counter = 0
             for i in value[::-1]: # to find time in the string, reverse the string for iterated lookup
                 if i == ' ':
@@ -41,32 +33,11 @@ def kafka_consumer():
                 else:
                     counter = counter + 1
 
-#            print('source ip ' + str(value[:10]) + 'num : ' + str(value[11:-counter])) + 'time : ' + str(value[-counter:])
             source_ip = decimal_to_human(str(value[:10]))
             pkt_num = str(value[11:-counter])
             time = str(value[-counter:])
             print('source ip : ' + source_ip + 'packet : ' + pkt_num) + 'time : ' + str(value[-counter:])
             insert_into_db(source_ip, pkt_num, time)
-            '''
-            if (len(value) == 30):
-#                print(value)
-                src_ip_0 = str(decimal_to_human(str(value[9:19])))
-                dst_ip_0 = str(decimal_to_human(str(value[20:30])))
-                print(' src1 : ' + str(src_ip_0) + 'dst1 : ' + str(dst_ip_0))
-                time_temp = '111'
-                insert_into_db(src_ip_0,dst_ip_0,time_temp)
-                
-            elif (len(value) == 60):
-#                print(value)
-                time_temp = '111'
-                src_ip_0 = str(decimal_to_human(str(value[9:19])))
-                dst_ip_0 = str(decimal_to_human(str(value[20:30])))
-                src_ip_1 = str(decimal_to_human(str(value[39:49])))
-                dst_ip_1 = str(decimal_to_human(str(value[50:60])))
-                print(' src1 : ' + str(src_ip_0) + ' dst1 : ' + str(dst_ip_0) + ' src2 : '+ str(src_ip_1) + ' dst2 : ' + str(dst_ip_1))
-                insert_into_db(src_ip_0,dst_ip_0,time_temp)
-                insert_into_db(src_ip_1,dst_ip_1,time_temp)
-            '''
     except KeyboardInterrupt:
         sys.exit()
 
@@ -79,7 +50,6 @@ def decimal_to_human(input_value):
     pt0 = literal_eval((str('0x'+str(hex_value[-8:-6]))))
     result = str(pt0)+'.'+str(pt1)+'.'+str(pt2)+'.'+str(pt3)
     return result
-
 
 kafka_consumer()
 
